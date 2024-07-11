19f55c6f627c4022c507a450a6903c02a6b3a961
Sha Jackson <shajack2000@Gmail.com>
2024-07-11 17:56:52 -0500

    Added more experimental loot spawn logic

--- levels/test_level/heist_zone/test_map_generator.gd
+++ levels/test_level/heist_zone/test_map_generator.gd
@@ -1,13 +1,20 @@
 extends Node3D
 
+var RNG : RandomNumberGenerator = RandomNumberGenerator.new()
+
 func _ready():
 	pass
 	
 func _spawn_loot():
-	var rooms = $DungeonGenerator3D.get_all_placed_and_preplaced_rooms()
+	var rooms : Array = $DungeonGenerator3D.get_all_placed_and_preplaced_rooms()
 	var banana_loot = load("res://loot/banana/banana.tscn")
 	var banana_loot_copy = banana_loot.instantiate()
-	pass
-	#for room in rooms:
-		#room.position.x
+	for room in rooms:
+		var x_variance = (room.voxel_scale.x * room.size_in_voxels.x / 2) - 0.5
+		var y_variance = (room.voxel_scale.y * room.size_in_voxels.y / 2) - 0.5
+		var loot_x = room.position.x + RNG.randf_range(-x_variance, x_variance)
+		var loot_y = room.position.y + RNG.randf_range(-y_variance, y_variance)
+		banana_loot_copy.translation.x = loot_x
+		banana_loot_copy.translation.y = loot_y
+		add_child(banana_loot_copy)
 		
