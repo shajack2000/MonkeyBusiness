eccdaed66fe4a18a0971421f4735a2a00eb4b520
Sha Jackson <shajack2000@Gmail.com>
2024-07-12 17:13:27 -0500

    Bananas spawn... somewhere

--- levels/test_level/heist_zone/test_map_generator.gd
+++ levels/test_level/heist_zone/test_map_generator.gd
@@ -1,20 +1,23 @@
 extends Node3D
 
 var RNG : RandomNumberGenerator = RandomNumberGenerator.new()
+var banana_loot : PackedScene = preload("res://loot/banana/banana.tscn")
 
 func _ready():
 	pass
 	
 func _spawn_loot():
 	var rooms : Array = $DungeonGenerator3D.get_all_placed_and_preplaced_rooms()
-	var banana_loot = load("res://loot/banana/banana.tscn")
-	var banana_loot_copy = banana_loot.instantiate()
+
 	for room in rooms:
+		var banana_loot_copy = banana_loot.instantiate()
+		print(banana_loot_copy)
 		var x_variance = (room.voxel_scale.x * room.size_in_voxels.x / 2) - 0.5
 		var y_variance = (room.voxel_scale.y * room.size_in_voxels.y / 2) - 0.5
 		var loot_x = room.position.x + RNG.randf_range(-x_variance, x_variance)
 		var loot_y = room.position.y + RNG.randf_range(-y_variance, y_variance)
-		banana_loot_copy.translation.x = loot_x
-		banana_loot_copy.translation.y = loot_y
+		banana_loot_copy.position.x = loot_x
+		banana_loot_copy.position.y = loot_y
+		banana_loot_copy.position.y = room.position.z + 5
 		add_child(banana_loot_copy)
 		
