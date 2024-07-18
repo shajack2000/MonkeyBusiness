b835b55345c00495d07ecd5ee71fcfe8190de50d
Sha Jackson <shajack2000@Gmail.com>
2024-07-17 19:18:50 -0500

    Merge loot spawning changes

--- levels/bank/main_menu.gd
+++ levels/bank/main_menu.gd
@@ -4,6 +4,14 @@ extends Control
 func _process(delta):
 	if $AudioStreamPlayer.playing == false:
 		$AudioStreamPlayer.play()
+=======
+<<<<<<< Updated upstream
+=======
+func _process(delta):
+	if $MenuMusicCool.playing == false:
+		$MenuMusicCool.play()
+>>>>>>> Stashed changes
+>>>>>>> loot_spawning
 
 func _on_new_game_pressed():
 	get_tree().change_scene_to_file("res://levels/test_level/heist_zone/test_scene.tscn")
